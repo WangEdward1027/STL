7a1a5909e0db4d26f99bf7701e815189ef75919d
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-07-01 22:28:55 +0800

    vector的构造、遍历、基本操作

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,4 +1,4 @@
-//vector的构造、遍历
+//vector的构造、遍历、基本操作
 
 #include <iostream> 
 #include <vector>
@@ -6,6 +6,7 @@ using std::cout;
 using std::endl;
 using std::vector;
 
+//1.vector的构造
 void test1(){
     //vector<int>代表一个特点的类
     //vector可以代表一类类,vector是类模板实现的
@@ -59,14 +60,75 @@ void test1(){
     cout << endl;
 }
 
+//2.vector的遍历
 void test2(){
+    vector<int> vec = {1,2,3,4,5};
+    //1.下标方式遍历
+    for(size_t idx = 0; idx < vec.size(); idx++){
+        cout << vec[idx] << " ";
+    }
+    cout << endl;
+
+    //2.增强for循环遍历
+    for(auto & ele : vec){
+        cout << ele << " ";
+    }
+    cout << endl;
+
+    //3.迭代器方式遍历
+    for(auto it = vec.begin(); it != vec.end(); ++it){
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+//3.vector的基本操作
+void test3(){
+    vector<int> vec = {1,2,3};
+    cout << "*vec.begin():" << *vec.begin() << endl;
+    cout << "*(vec.end()-1):" << *(vec.end()-1) << endl;
+
+    vec.push_back(4);
+    cout << "vec.size():" << vec.size() << endl;
+    cout << "vec.capacity():" << vec.capacity() << endl;
+
+    vec.pop_back();
+    vec.pop_back();
+    vec.pop_back();
+    vec.pop_back();
+    cout << "vec.empey():" << vec.empty() << endl;
+    cout << "vec.size():" << vec.size() << endl;
+    cout << "vec.capacity():" << vec.capacity() << endl;
+    
+    for(int i = 1; i < 8; i++){
+        vec.push_back(i);
+    }
+    cout << "vec.size():" << vec.size() << endl;
+    cout << "vec.capacity():" << vec.capacity() << endl;
+    
+    vec.clear();
+    cout << "vec.size():" << vec.size() << endl;
+    cout << "vec.capacity():" << vec.capacity() << endl;
+
+    vec.shrink_to_fit();
+    cout << "vec.size():" << vec.size() << endl;
+    cout << "vec.capacity():" << vec.capacity() << endl;
+
+    vec.reserve(100);
+    cout << "vec.size():" << vec.size() << endl;
+    cout << "vec.capacity():" << vec.capacity() << endl;
+}
+
+//4.vector的自动扩容
+void test4(){
     vector<int> vec8(5,8);
     cout << "nums's size: " << vec8.size() << endl;
     cout << "nums's capacity: " << vec8.capacity() << endl;
     cout << sizeof(vec8) << endl;
 }
 
-void test3(){
+//5.在堆上创建vector对象
+void test5(){
     //vector对象可以创建在栈上,也可以创建在堆上
     vector<int> * vec = new vector<int>();
     vec->push_back(10);
@@ -75,7 +137,8 @@ void test3(){
 
 int main()
 {
-    test1();   
-    /* test2(); */   
+    /* test1(); */   
+    test2();   
+    /* test3(); */   
     return 0;
 }
