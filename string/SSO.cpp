e2826a234d7ad108fa6a26c2a3b967e94c27ac33
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-07-09 10:18:26 +0800

    string的第三种实现:短字符串优化 SSO

--- string/SSO.cpp
+++ string/SSO.cpp
@@ -32,64 +32,80 @@ private:
 };
 
 String::String(const char * pstr)
-:_size(strlen(pstr)+1)
+:_size(strlen(pstr))
 {
     if(_size<=15){
         _capacity = 15;
+        memset(_buffer._local, 0, sizeof(_buffer._local));
         strcpy(_buffer._local, pstr);
     }else{ //_size > 15
         _capacity = _size;
-        _buffer._pointer = new char[_size];
+        _buffer._pointer = new char[strlen(pstr)+1];
         strcpy(_buffer._pointer, pstr);
     }
     cout << "String(const char * pstr)" << endl;
 }
 
+//union会导致_pointer不再是空指针
 String::~String(){
     if(_size > 15){
-        if(_buffer._pointer){
+        /* if(_buffer._pointer){ */
             delete [] _buffer._pointer;
             _buffer._pointer = nullptr;
-        }
+        /* } */
     }
     cout << "~String()" << endl;
 }
 
 //成员函数重载
 char & String::operator[](size_t idx){
+    if(idx > _size - 1){
+        cout << "out of range" << endl; 
+             static char nullchar = '\0'; 
+             return nullchar; 
+    }
     if(_size <= 15){
-        size_t len = strlen(_buffer._local);
-        if(idx < len){
-            return _buffer._local[idx];
-        }else{
-            cout << "out of _local range" << endl;
-            static char nullchar = '\0';
-            return nullchar;
-        }
+        return _buffer._local[idx];
     }else{ //_size > 15
-        size_t len = strlen(_buffer._pointer);
-        if(idx < len){
-            return _buffer._pointer[idx];
-        }else{
-            cout << "out of _pointer range" << endl;
-            static char nullchar = '\0';
-            return nullchar;
-        }
+        return _buffer._pointer[idx];
     }
     cout << "operator[](size_t idx)" << endl;
 }
 
+/* //成员函数重载 */
+/* char & String::operator[](size_t idx){ */
+/*     if(_size <= 15){ */
+/*         size_t len = strlen(_buffer._local); */
+/*         if(idx < len){ */
+/*             return _buffer._local[idx]; */
+/*         }else{ */
+/*             cout << "out of _local range" << endl; */
+/*             static char nullchar = '\0'; */
+/*             return nullchar; */
+/*         } */
+/*     }else{ //_size > 15 */
+/*         size_t len = strlen(_buffer._pointer); */
+/*         if(idx < len){ */
+/*             return _buffer._pointer[idx]; */
+/*         }else{ */
+/*             cout << "out of _pointer range" << endl; */
+/*             static char nullchar = '\0'; */
+/*             return nullchar; */
+/*         } */
+/*     } */
+/*     cout << "operator[](size_t idx)" << endl; */
+/* } */
+
 //友元函数重载
 ostream & operator<<(ostream & os,const String & rhs){
     if(rhs._size > 15){
-        if(rhs._buffer._pointer){
+        /* if(rhs._buffer._pointer){ */
             os << rhs._buffer._pointer;
-        }
-        return os;
+        /* } */
     }else{
         os << rhs._buffer._local;
-        return os;
     }
+    return os;
 }
 
 size_t String::size(){
@@ -102,7 +118,7 @@ size_t String::capacity(){
 
 void test0(){
     String str1("hello");
-    String str2("hello,world!!!!");
+    String str2("hello,world!!!!!");
     cout << str1.size() << endl;
     cout << str1.capacity() << endl;
     cout << str2.size() << endl;
